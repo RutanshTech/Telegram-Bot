679d3dc04353fd13ba83ac33b5d713667f2b0bd3
mandavra <141617041+mandavra@users.noreply.github.com>
2025-06-23 11:09:37 +0530

    Delete fontend directory

--- fontend/src/Adminpanel/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/ProtectedRoute.jsx
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem("auth") === "true";
-  return isAuthenticated ? children : <Navigate to="/loginAdmin" replace />;
-};
-
-export default ProtectedRoute;
